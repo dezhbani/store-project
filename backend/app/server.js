804171f0c8d7b5041adbd01f2c1994c20d2efd7f
matin dezhbani <dezhbani.matin@gmail.com>
2022-05-23 13:37:40 +0430

    in it redis in project

--- backend/app/server.js
+++ backend/app/server.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const createError = require("http-errors");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJS = require("swagger-jsdoc");
+const cors = require("cors");
 
 module.exports = class Application{
     #app = express();
@@ -16,12 +17,14 @@ module.exports = class Application{
         this.#PORT = PORT;
         this.#DB_URL = DB_URL;
         this.configApplication();
+        // this.initRedis();
         this.connectToDB();
         this.createServer();
         this.createRoutes();
         this.errorHandling();
     }
     configApplication(){
+        this.#app.use(cors())
         this.#app.use(morgan("dev"));
         this.#app.use(express.json());
         this.#app.use(express.urlencoded({extended: true}));
@@ -54,19 +57,22 @@ module.exports = class Application{
     connectToDB(){
         mongoose.connect(this.#DB_URL, (error) => {
             if (!error) return console.log("conected to MongoDB");
-            return console.log(error.message);
-        });
-        mongoose.connection.on("connected", () => {
-        console.log("mongoose connected to DB");
-        });
-        mongoose.connection.on("disconnected", () => {
-        console.log("mongoose connection is disconnected");
-        });
-        process.on("SIGINT", async () => {
-        await mongoose.connection.close();
-        console.log("disconnected");
-        process.exit(0);
-        });
+            return console.log(error);
+          });
+          mongoose.connection.on("connected", () => {
+            console.log("mongoose connected to DB");
+          });
+          mongoose.connection.on("disconnected", () => {
+            console.log("mongoose connection is disconnected");
+          });
+          process.on("SIGINT", async () => {
+            await mongoose.connection.close();
+            console.log("disconnected");
+            process.exit(0);
+          });
+    }
+    initRedis(){
+        require("./utils/init-redis")
     }
     createRoutes(){
         this.#app.use(AllRoutes)
