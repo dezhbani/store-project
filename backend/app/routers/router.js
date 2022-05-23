b14557521705450597213b311f33f03d0d3dcd25
matin dezhbani <dezhbani.matin@gmail.com>
2022-05-24 00:30:11 +0430

    add developer route

--- backend/app/routers/router.js
+++ backend/app/routers/router.js
@@ -1,9 +1,18 @@
+const redisClient = require("../utils/init-redis");
 const  {HomeRoutes}  = require("./api");
+const { DeveloperRoute } = require("./developer.routes");
 const { userAuthRoutes } = require("./user/auth");
 
 const router = require("express").Router();
 
+(async() => {
+    await redisClient.set("key", "value")
+    const value = await redisClient.get("key")
+    console.log(value)
+})()
+
 router.use("/user", userAuthRoutes);
+router.use("/developer", DeveloperRoute)
 router.use("/", HomeRoutes);
 
 module.exports = {
