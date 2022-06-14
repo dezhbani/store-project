1f9b0c89b095d2952c3451771526dfcc2cac347f
matin dezhbani <dezhbani.matin@gmail.com>
2022-06-14 21:37:06 +0430

    create function for convert string to array

---
+++ app/http/middlewares/stringToArray.js
@@ -0,0 +1,20 @@
+const stringToArray = (field) =>{
+    return function(req, res, next){
+        if(req.body[field]){
+            if(typeof req.body[field] == "string"){
+                if(req.body[field].indxOf("#") >= 0){
+                    req.body[field] = req.body[field].split("#").map(item => item.trim())
+                }
+            }else if((req.body[field].constructor).toString().toLowerCase().indxOf("array") >= 0){
+                req.body[field] = req.body[field].map(item => item.trim())
+            }
+        }else{
+            req.body[field] = [];
+        }
+        next()
+    }
+}
+
+module.exports = {
+    stringToArray
+}
