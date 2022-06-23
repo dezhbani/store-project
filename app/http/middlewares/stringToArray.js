2a32306b5f7357c781e4f61d5a1158ca7deb46b7
matin dezhbani <dezhbani.matin@gmail.com>
2022-06-23 12:08:16 +0430

    edit middleware & model

--- app/http/middlewares/stringToArray.js
+++ app/http/middlewares/stringToArray.js
@@ -2,8 +2,9 @@ const stringToArray = (field) =>{
     return function(req, res, next){
         if(req.body[field]){
             if(typeof req.body[field] == "string"){
-                if(req.body[field].indxOf("#") >= 0){
-                    req.body[field] = req.body[field].split("#").map(item => item.trim())
+                if(req.body[field].indexOf("#") >=0){
+                    req.body[field] = req.body[field].split("#").map(item => item.trim());
+                    req.body[field].shift();
                 }
             }else if((req.body[field].constructor).toString().toLowerCase().indxOf("array") >= 0){
                 req.body[field] = req.body[field].map(item => item.trim())
--- app/http/models/blogs.js
+++ app/http/models/blogs.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const CommentSchema = new mongoose.Schema({
     user: {type: mongoose.Types.ObjectId, ref: "users", required: true},
     comment: {type: String, required: true},
-    createAt: {type: Date, default: Date.now()},
+    createAt: {type: Date, default: new Date().getTime()},
     parent: {type: mongoose.Types.ObjectId}
 })
 
