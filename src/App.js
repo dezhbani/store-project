b6fc3ba6dfeabdaa645f8170b3959d541c3ced0e
matin dezhbani <dezhbani.matin@gmail.com>
2022-08-11 09:42:53 +0430

    commit for push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './admin/components/Dashboard';
+import style from "./App.module.css";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
+        <Routes>
+        {/* <Route path="/dashboard" element={<Dashboard/>}/> */}
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './admin/components/Dashboard';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+<BrowserRouter>
+<App/>
+<Dashboard/>
+</BrowserRouter>
+)
+
+
